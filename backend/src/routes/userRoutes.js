b4798dad19dff0296c1323693790ad8ec8c5f1d1
Chamath <chamaththilakarathna10@gmail.com>
2025-03-03 09:31:21 +0530

    Jwt authentication implemented

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,8 +6,7 @@ const userAuth = require("../middleware/authMiddleware");
 const refreshTokenMiddleware = require("../middleware/refreshMiddleware");
 
 router.post("/signup", register);
-router.post("/signin/admin", (req, res) => authController.login(req, res, "admin"));
-router.post("/signin/customer", (req, res) => authController.login(req, res, "customer")); 
+router.post("/signin", login);
 router.post("/refresh-token", refreshToken); 
 router.post("/signout", logout); 
 
